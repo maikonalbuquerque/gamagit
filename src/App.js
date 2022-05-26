6d6f0b5e388f5cd4bf5b4fb51fc73aa75831c21a
Maikon Albuquerque <maikonmorais@gmail.com>
2022-05-26 01:37:18 -0300

    Iniciando o projeto Gama Git em React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
+import React, { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const [usuario, setUsuario] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <>  
+            <p>{ usuario }</p>
+            <input className="usuarioInput" placeholder="usuário" onChange = {e => setUsuario(e.target.value)}/>
+            <button type="button">Pesquisar</button>
+        </>
   );
 }
 
